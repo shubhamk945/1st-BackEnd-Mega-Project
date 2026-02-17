5554c61d10d72a1fdd15ac632c97186bea9b369e
shubhamk945 <shubhamk6777@gmil.com>
2026-02-17 18:43:54 +0530

    Connect to mongo atlias

---
+++ src/db/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import 'dotenv/config'
+
+
+const connectDB = async () => {
+    if (!process.env.MONGODB_URI) {
+        console.error("MONGODB_URI is not defined in the environment variables. Please set it in your .env file.");
+        process.exit(1);
+    }
+    try{
+     const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    //  const connectionInstance =  await mongoose.connect(`mongodb+srv://shubhamk6777_db_user:shubham123@cluster0.x39vy1h.mongodb.net/?retryWrites=true&w=majority`)
+     console.log(`\n MongoDB connected !! DB HOST: 
+        ${connectionInstance.connection.host}`);
+     
+    } catch(error){
+        console.log("MONGODB CONNECTION error ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
+
+
+// shubhamk6777_db_user
+// shubham123
